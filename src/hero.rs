f79c2755b96bf66eeef2722b8cee45a59536b605
idorocodes <idoroyen33@gmail.com>
2026-01-07 00:23:52 +0100

    wait a little

--- src/hero.rs
+++ src/hero.rs
@@ -1,3 +1,5 @@
+use std::{thread, time::Duration};
+
 use leptos::prelude::*;
 
 
@@ -5,8 +7,18 @@ use leptos::prelude::*;
 
 pub fn Hero () -> impl IntoView {
     
+    let (visible, set_visible) = signal(false);
+    
+    let timeout_cb = move || {
+        set_visible.set(true);
+    };
+    
+    set_timeout(timeout_cb, Duration::from_secs(1));
     
     view! {
-        <h1> "This is the Hero Section" </h1>
-    }
+        {move || visible.get().then(|| view! {
+            <h1>"This is the Hero Section"</h1>
+        })}
+        
+    } 
 }
